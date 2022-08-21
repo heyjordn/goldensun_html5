263c3062e1c4f6127444b9a23a1c40754e5558d9
heyjordn <proxima.aust@gmail.com>
2022-08-20 19:53:39 -0500

    feat, added sample jest tests

---
+++ tests/index.test.ts
@@ -0,0 +1,9 @@
+import {describe, expect, test} from '@jest/globals'
+import { Hero } from "../base/index";
+
+describe("It can run a test", () => {
+    test("Testing test", () => {
+        expect(true).toBe(true);
+    })
+});
+
